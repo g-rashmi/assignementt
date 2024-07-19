92c41d8573798de632832003ba54cc35491145f7
g-rashmi <rashmi.gupta7018@gmail.com>
2024-07-20 01:41:46 +0530

    finall

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './components/Signup';
 import Signin from './components/Signin';
-import Navbarr from './components/Navbarr';
 import Order from './components/Order';
 
 function App() {
